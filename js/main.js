2a22625d21384f9c555e85e73c2ea7dfc2ec8ef1
dustfoot <dustfoot3668@gmail.com>
2019-11-05 09:04:51 +0300

    debugging main.js file

--- js/main.js
+++ js/main.js
@@ -28,28 +28,28 @@ $(document).ready(function () {
    });
    $(document).ready(function () {
       $('.work1Logo').hover(function () {
-         console.log('hovering')
+         alert('hovering')
          $("#work1Text").show();
       });
    });
 });
-console.log('it works')
+alert('it works')
 $(document).ready(function () {
    $('.submit').click(function (event) {
       event.preventDefault();
-      console.log('clicked button');
+      alert('clicked button');
       var name = document.getElementById('nameInput').value;
       var email = document.getElementById('exampleFormControlInput1').value;
       var message = document.getElementById('messageInput').value;
       var statusElm = document.getElementById('status');
 
       if (email.length > 5 && email.includes('@') && email.includes('.')) {
-         console.log("email valid")
+         alert("email valid")
       } else {
-         console.log("email invalid")
+         alert("email invalid")
       }
       if (name.length > 2) {
-         console.log(name);
+         alert(name);
       } else {
          console.log('name invalid')
       }
